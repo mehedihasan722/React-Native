42a62d04c1e38ec887b45085ad2ebae6e30c58bd
mehedihasan722 <mjimehedi99@gmail.com>
2025-05-26 06:40:54 +0600

    🦺 Button Component Complete
    Nothing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,10 @@
-import { Image, Text, View } from "react-native";
-
-
-const logoImg = require("../../assets/images/adaptive-icon.png");
+import { Button, View } from "react-native";
 
 
 export default function HomeScreen() {
   return (
-    <View style={{flex:1, backgroundColor: "plum", padding:50, alignItems:"center"}} >
-      <Text style={{color: "blue", fontSize:30}}> Hello World </Text>
-      <Image source={logoImg} style={{height:200, width:200}} />
-      <Image source={{uri:"https://picsum.photos/seed/picsum/200/300"}} style={{height:200, width:300}} />
+    <View style={{flex:1, backgroundColor: "plum", padding:50}} >
+      <Button title="Click Me" onPress={()=> console.log('Button Press')} color="midnightblue" disabled />
     </View>
   )
 }
