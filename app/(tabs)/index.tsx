842c0a8081f9e1c5dbe87c18f3dbe297966a498d
mehedihasan722 <mjimehedi99@gmail.com>
2025-05-26 07:06:53 +0600

    🐛 Activity Indicator Completed
    fgs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
-import { StatusBar, View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 
 
 export default function HomeScreen() {
 
   return (
     <View style={{flex:1, backgroundColor: "plum", padding:50}} >
-      <StatusBar backgroundColor="lightgreen" barStyle="dark-content" hidden />
+      <ActivityIndicator  />
+      <ActivityIndicator size="large"  />
+      <ActivityIndicator size="large" color="midnightblue"  />
+      <ActivityIndicator size="large" color="midnightblue" animating={false} />
     </View>
   )
 }
