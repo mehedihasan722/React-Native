25303ab7bfa4940bd3534ca20ae3f31103b0ee70
mehedihasan722 <mjimehedi99@gmail.com>
2025-05-26 07:13:17 +0600

    💄 Alert Completed
    fgsfg

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,23 @@
-import { ActivityIndicator, View } from "react-native";
+import { Alert, Button, View } from "react-native";
 
 
 export default function HomeScreen() {
 
   return (
     <View style={{flex:1, backgroundColor: "plum", padding:50}} >
-      <ActivityIndicator  />
-      <ActivityIndicator size="large"  />
-      <ActivityIndicator size="large" color="midnightblue"  />
-      <ActivityIndicator size="large" color="midnightblue" animating={false} />
+      <Button title="Alert" onPress={()=>Alert.alert("InValid Data!")} />
+      <Button title="Alert 2" onPress={()=>Alert.alert("InValid Data!","Dob Incorrect")} />
+      <Button title="Alert 3" onPress={()=>Alert.alert("InValid Data!","Dob Incorrect" , [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        {
+          text: "OK",
+          onPress: () => console.log("OK Pressed")
+        }
+      ])} />
     </View>
   )
 }
