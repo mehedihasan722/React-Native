740ed14bd08d526829cd4348a26e4f72aa907817
mehedihasan722 <mjimehedi99@gmail.com>
2025-05-26 06:54:12 +0600

    📝 Modal Basic Completed
    fgahn

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
-import { Button, View } from "react-native";
+import { useState } from "react";
+import { Button, Modal, Text, View } from "react-native";
 
 
 export default function HomeScreen() {
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{flex:1, backgroundColor: "plum", padding:50}} >
-      <Button title="Click Me" onPress={()=> console.log('Button Press')} color="midnightblue" disabled />
+      <Button title="Click Me" onPress={()=> setIsModalVisible(true)} color="midnightblue"  />
+        <Modal visible={isModalVisible} onRequestClose={()=> setIsModalVisible(false)} animationType="slide" presentationStyle="pageSheet">
+          <View style={{flex:1, backgroundColor: "lightblue", padding:50}} >
+            <Text>Modal Content</Text>
+            <Button title="Close Modal" onPress={()=> setIsModalVisible(false)} color="midnightblue"   />
+          </View> 
+        </Modal>
     </View>
   )
 }
