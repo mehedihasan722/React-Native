1e8e08e47c11d1ee8adc2b9a239d88122f14b364
mehedihasan722 <mjimehedi99@gmail.com>
2025-05-26 07:03:26 +0600

    🚑️ Status Bar Completed
    hkfsdg

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
-import { Button, Modal, Text, View } from "react-native";
+import { StatusBar, View } from "react-native";
 
 
 export default function HomeScreen() {
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{flex:1, backgroundColor: "plum", padding:50}} >
-      <Button title="Click Me" onPress={()=> setIsModalVisible(true)} color="midnightblue"  />
-        <Modal visible={isModalVisible} onRequestClose={()=> setIsModalVisible(false)} animationType="slide" presentationStyle="pageSheet">
-          <View style={{flex:1, backgroundColor: "lightblue", padding:50}} >
-            <Text>Modal Content</Text>
-            <Button title="Close Modal" onPress={()=> setIsModalVisible(false)} color="midnightblue"   />
-          </View> 
-        </Modal>
+      <StatusBar backgroundColor="lightgreen" barStyle="dark-content" hidden />
     </View>
   )
 }
